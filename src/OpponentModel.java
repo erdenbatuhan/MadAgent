afcdd44cc9134ac9c530c946c9c4c297665731d1
Ekrem Çetinkaya <ekrem.cetinkaya@ozu.edu.tr>
2017-04-16 13:50:23 +0300

    Updated to Genius 7.1.1

---
+++ src/OpponentModel.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import list.Tuple;
+import negotiator.AgentID;
+import negotiator.Bid;
+import negotiator.Deadline;
+import negotiator.actions.Accept;
+import negotiator.actions.Action;
+import negotiator.actions.Offer;
+import negotiator.parties.AbstractNegotiationParty;
+import negotiator.persistent.PersistentDataContainer;
+import negotiator.persistent.PersistentDataType;
+import negotiator.persistent.StandardInfo;
+import negotiator.persistent.StandardInfoList;
+import negotiator.timeline.TimeLineInfo;
+import negotiator.utility.AbstractUtilitySpace;
+
+public class OpponentModel {
+	private ArrayList<Offer> lastBids;
+
+}
